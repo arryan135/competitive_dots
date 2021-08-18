8e2a5a5e0b5ec7b7f7489fc5d2201e9d946a6e10
arryan <arryan@umich.edu>
2021-08-17 20:20:03 -0400

    allow dots to move in a particular direction

--- public/canvasStuff.js
+++ public/canvasStuff.js
@@ -6,6 +6,19 @@ player.locX = Math.floor(500*Math.random() + 100);
 player.locY = Math.floor(500*Math.random() + 100);
 
 const draw = () => {
+  
+  // wipe out the entire canvas everytime draw is called
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  // reset the translate done in the previous draw call
+  context.setTransform(1, 0, 0, 1, 0, 0);
+
+  // clamp the camera to the player 
+  const camX = -player.locX + canvas.width/2;
+  const camY = -player.locY + canvas.height/2;
+  // allos us to move canvas around
+  context.translate(camX, camY);
+
   // tell the context to start drawing
   context.beginPath();
   context.fillStyle = `rgb(255,0,0)`;
@@ -14,6 +27,7 @@ const draw = () => {
   // argument 4 where to start drawing the circle in radians
   // argument 5 is where to stop drawing the circle in radians
   context.arc(player.locX, player.locY, 10, 0, Math.PI*2);
+  context.arc(200, 200, 10, 0, Math.PI*2);
   // fill the circle
   context.fill();
   context.lineWidth = 3;
@@ -59,4 +73,4 @@ canvas.addEventListener('mousemove', event => {
       player.locX += speed * xV;
       player.locY -= speed * yV;
   }    
-})
+});
